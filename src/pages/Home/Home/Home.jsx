5899feeaf35b6ff3ae42ebf6d331588f0a1cec1f
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-06-26 03:03:38 +0600

    added contact section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Bot from '../Bot/Bot';
 import Banner from '../Banner/Banner';
+import Contact from '../../Sheared/Contact/Contact';
 
 const Home = () => {
     return (
         <div>
             <Bot></Bot>
             <Banner></Banner>
+            <Contact></Contact>
         </div>
     );
 };
