b2676066f91bb6f3ad7f0ed498ebcc2a8de7c9ac
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-06-26 03:14:59 +0600

    hire me page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Bot from '../Bot/Bot';
 import Banner from '../Banner/Banner';
 import Contact from '../../Sheared/Contact/Contact';
+import HireMe from '../HireMe/HireMe';
 
 const Home = () => {
     return (
         <div>
             <Bot></Bot>
             <Banner></Banner>
+            <HireMe></HireMe>
             <Contact></Contact>
+            
         </div>
     );
 };
